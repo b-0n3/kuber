93830a5f237d99d9b82ff3b1fe242c08e319b502
b_one <lalouabd123@gmail.com>
2020-03-03 11:18:50 +0100

    al

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,5 @@
 FROM debian:buster
 
+COPY ./srcs/HolderSetup.sh ./HolderSetup.sh
+RUN chmod +x HolderSetup.sh
+RUN /bin/bash HolderSetup.sh
--- srcs/HolderSetup.sh
+++ srcs/HolderSetup.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
 
 
-apt-get update;
-sudo apt-get install \
- apt-transport-https \
-	ca-certificates \
-	curl \
-	git \
-	gnupg-agent \
-	software-properties-common;
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -;
-apt-key fingerprint 0EBFCD88;
-add-apt-repository \
-	   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-	      $(lsb_release -cs) \
-		     stable";
-			 
-apt-get update;
-apt-get install docker-ce docker-ce-cli containerd.io;
-echo "installing docker [Ok]"
-echo "installin kuber  >>>"
 apt -y install qemu-kvm libvirt-daemon libvirt-daemon-system virtinst bridge-utils;
 apt -y install apt-transport-https gnupg2 curl ;
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
@@ -30,6 +11,4 @@ echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/so
  wget https://storage.googleapis.com/minikube/releases/latest/docker-machine-driver-kvm2;
  chmod 755 minikube docker-machine-driver-kvm2;
  mv minikube docker-machine-driver-kvm2 /usr/local/bin/;
-echo "installing kuber [ok]"
  
-
