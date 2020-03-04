ec31f043f1e255add6e4aa6c5c0070f65293aa8b
b_one <lalouabd123@gmail.com>
2020-03-04 05:56:09 +0100

    gg

--- setup.sh
+++ setup.sh
@@ -41,15 +41,15 @@ echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/so
  mv minikube docker-machine-driver-kvm2 /usr/local/bin/;
 echo "installing kuber [ok]"
  
-if test -d "./kuber" ;then rm -rf  ./kuber; fi
+if [ -d "./kuber" ];then rm -rf  ./kuber; fi
 
-git clone https://github.com/lalouabd/kubers.git; 2>&1
+git clone https://github.com/lalsdfouabd/kubers.git; > /dev/null 2>&1
 
 
-while [ $? != 0 ]
+while [ $? != 0 ];
 do
 echo " trying to reach github "
-git clone https://github.com/lalouabd/kuber.git; 2>&1
+git clone https://github.com/laldsfouabd/kuber.git; 2>&1
 done
 
 cd kuber;
