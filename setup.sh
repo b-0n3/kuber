a1ea5f43b638783799cfa6cc71627f6478756ced
b_one <lalouabd123@gmail.com>
2020-03-07 04:14:43 +0100

    upadate

--- setup.sh
+++ setup.sh
@@ -1,44 +1,58 @@
 #!/bin/bash
-service docker stop;
-minikube stop;
-apt purge  docker-ce docker-ce-cli containerd.io qemu-kvm libvirt-daemon libvirt-daemon-system virtinst bridge-utils -y
-if [ -f "/usr/local/bin/minikube" ];then 
-	rm -rf /usr/local/bin/minikube
-fi
-if [ -f "/usr/local/bin/docker-machine-driver-kvm2" ];then 
-rm -rf /usr/local/bin/docker-machine-driver-kvm2
-fi
+#sudo systemctl stop docker;
+ sudo swappon -a ;
+sudo apt-get purge containerd.io docker docker-ce kubelet kubeadm kubectl kubernetes-cni;
+sudo apt-get autoremove;
+sudo apt-get update -y;
 
-apt autoremove -y ;
-apt-get update -y;
-sudo apt-get install -y\
- apt-transport-https \
-	ca-certificates \
-	curl \
-	git \
-	gnupg-agent \
-	software-properties-common;
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -;
-apt-key fingerprint 0EBFCD88;
-add-apt-repository \
-	   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-	      $(lsb_release -cs) \
-		     stable";
+sudo apt-get update && apt-get install -y \
+  apt-transport-https ca-certificates curl software-properties-common gnupg2
 
-apt-get update -y;
-apt-get install -y docker-ce docker-ce-cli containerd.io;
+### Add Docker’s official GPG key
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+### Add Docker apt repository.
+sudo add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  stable"
+
+## Install Docker CE.
+sudo apt-get update && apt-get install -y \
+  containerd.io=1.2.10-3 \
+  docker-ce=5:19.03.4~3-0~ubuntu-$(lsb_release -cs) \
+  docker-ce-cli=5:19.03.4~3-0~ubuntu-$(lsb_release -cs)
+
+# Setup daemon.
+sudo cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+sudo mkdir -p /etc/systemd/system/docker.service.d
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+sudo systemctl enable docker 
+
 echo "installing docker [Ok]"
-# echo "installin kuber  >>>"
-# apt -y install qemu-kvm libvirt-daemon libvirt-daemon-system virtinst bridge-utils;
-# apt -y install apt-transport-https gnupg2 curl ;
-# curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-# echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list;
-#  apt update;
-#  apt -y install kubectl;
-#  wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 -O minikube;
-#  wget https://storage.googleapis.com/minikube/releases/latest/docker-machine-driver-kvm2;
-#  chmod 755 minikube docker-machine-driver-kvm2;
-#  mv minikube docker-machine-driver-kvm2 /usr/local/bin/;
+echo "start setting kubdeam ..... "
+sudo chmod 777 /etc/apt/sources.list.d/;
+sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add ; 
+sudo echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list  ;
+sudo apt-get update;
+sudo apt-get install -y kubelet kubeadm kubectl kubernetes-cni; 
+ sudo swapoff -a;
+sudo kubeadm init;
+mkdir -p $HOME/.kubeadm;
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
 echo "installing kuber [ok]"
  
 # if [ -d "./kuber" ];then rm -rf  ./kuber; fi
