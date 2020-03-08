ca932a16a1d9c66251db47b34587d06568c77162
b_one <lalouabd123@gmail.com>
2020-03-08 04:31:21 +0100

    aa

--- setup.sh
+++ setup.sh
@@ -48,77 +48,77 @@ clearn ;
 
 sudo apt-get update -y;
 
-sudo apt-get update && apt-get install -y  apt-transport-https ca-certificates curl software-properties-common gnupg2
+# sudo apt-get update && apt-get install -y  apt-transport-https ca-certificates curl software-properties-common gnupg2
 
-### Add Docker’s official GPG key
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# ### Add Docker’s official GPG key
+# sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-### Add Docker apt repository.
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+# ### Add Docker apt repository.
+# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
-## Install Docker CE.
-sudo apt-get update ;
- sudo apt-get install -y containerd.io docker-ce docker-ce-cli ;
+# ## Install Docker CE.
+# sudo apt-get update ;
+#  sudo apt-get install -y containerd.io docker-ce docker-ce-cli ;
 
-# Setup daemon.
-sudo cat > /etc/docker/daemon.json <<EOF
-{
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "100m"
-  },
-  "storage-driver": "overlay2"
-}
-EOF
+# # Setup daemon.
+# sudo cat > /etc/docker/daemon.json <<EOF
+# {
+#   "exec-opts": ["native.cgroupdriver=systemd"],
+#   "log-driver": "json-file",
+#   "log-opts": {
+#     "max-size": "100m"
+#   },
+#   "storage-driver": "overlay2"
+# }
+# EOF
 
 
 
-sudo systemctl daemon-reload
-sudo systemctl restart docker
-sudo systemctl enable docker 
+# sudo systemctl daemon-reload
+# sudo systemctl restart docker
+# sudo systemctl enable docker 
 
-echo "installing docker [Ok]"
-echo "start setting kubdeam ..... "
-sudo chmod 777 /etc/apt/sources.list.d/;
-sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add ; 
-sudo echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list ;
-sudo apt-get update;
-sudo apt-get install -y kubelet kubeadm kubectl kubernetes-cni; 
+# echo "installing docker [Ok]"
+# echo "start setting kubdeam ..... "
+# sudo chmod 777 /etc/apt/sources.list.d/;
+# sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add ; 
+# sudo echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list ;
+# sudo apt-get update;
+# sudo apt-get install -y kubelet kubeadm kubectl kubernetes-cni; 
 
-sudo swapoff -a;
+# sudo swapoff -a;
 
-sudo systemctl daemon-reload
+# sudo systemctl daemon-reload
 
- sudo systemctl restart kubelet
+#  sudo systemctl restart kubelet
 
-sudo kubeadm init;
+# sudo kubeadm init;
 
-sudo apt-mark hold kubelet kubeadm kubectl;
-mkdir -p $HOME/.kube/;
+# sudo apt-mark hold kubelet kubeadm kubectl;
+# mkdir -p $HOME/.kube/;
 
-sudo  cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+# sudo  cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
+# sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-echo "installing kuber [ok]"
+# echo "installing kuber [ok]"
  
- if [ -d "./kuber" ];then sudo rm -rf  ./kuber; fi
+#  if [ -d "./kuber" ];then sudo rm -rf  ./kuber; fi
 
- git clone https://github.com/lalouabd/kuber.git; 2>&1
-
-
-# while [ $? != 0 ];
-# do
-# echo " trying to reach github "
-# git clone https://github.com/lalouabd/kuber.git; 2>&1
-# done
-
- cd kuber;
-
-sudo docker build ./srcs/pods/mysql/  -t mysql-deployment;
-sudo docker build ./srcs/pods/server/ -t myserver;
+#  git clone https://github.com/lalouabd/kuber.git; 2>&1
 
 
-sudo kubectl apply -f srcs/pods/mysql/srcs/mysql.yaml &> /dev/null
+# # while [ $? != 0 ];
+# # do
+# # echo " trying to reach github "
+# # git clone https://github.com/lalouabd/kuber.git; 2>&1
+# # done
+
+#  cd kuber;
+
+# sudo docker build ./srcs/pods/mysql/  -t mysql-deployment;
+# sudo docker build ./srcs/pods/server/ -t myserver;
+
+
+# sudo kubectl apply -f srcs/pods/mysql/srcs/mysql.yaml &> /dev/null
 
